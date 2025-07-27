346b58459293acc243c90d6fa8bddec38884e8ac
429 <a429@MacBook-Pro.local>
2025-07-27 21:46:20 +0300

    fied burger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
     >
       <div className="navbar-container flex items-center justify-between gap-10  ">
    
-        <div className='flex gap-5 items-center'>
+        <div className='logo-img flex gap-5 items-center justify-center'>
             <div className='burger-button' onClick={() => setIsOpen(!isOpen)}>
                 <span className={`first ${isOpen? 'open':'first'}`}></span>
                 <span className={`second ${isOpen? 'open':'second'}`}></span>
@@ -61,10 +61,10 @@ const Navbar = () => {
           <img src={logoPict} alt="Logo" className="h-8" />
         </div>
         <ul  className={`navbar-list flex items-center gap-6 ${isOpen ? 'open' : ''}`}>
-          <li><a href="#" className="navbar-link px-4">Головна</a></li>
-          <li><a href="#" className="navbar-link px-4">Про нас</a></li>
-          <li><a href="#" className="navbar-link px-4">Послуги</a></li>
-          <li><a href="#" className="navbar-link px-4">Контакти</a></li>
+          <li><a href="#" className="navbar-link px-4" onClick={()=>setIsOpen(false)}>Головна</a></li>
+          <li><a href="#" className="navbar-link px-4" onClick={()=>setIsOpen(false)}>Про нас</a></li>
+          <li><a href="#" className="navbar-link px-4" onClick={()=>setIsOpen(false)}>Послуги</a></li>
+          <li><a href="#" className="navbar-link px-4" onClick={()=>setIsOpen(false)}>Контакти</a></li>
         </ul>
         <div>
           <a href="#" className="button px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition duration-300">Приєднатися</a>
