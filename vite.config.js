dc66d6b454212f9bcef51be1119d66d8440be6e3
429 <a429@MacBook-Pro.local>
2025-09-24 18:20:19 +0300

    dockerfile

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/",
-  preview: {
+  base:"/indi_b2b/",
+    preview: {
     host: '0.0.0.0',
     port: 4173,
     allowedHosts: ['b2b.indiapp.com.ua']
-  }
+    }
 })
-
