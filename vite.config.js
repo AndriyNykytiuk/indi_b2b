e668c38f32b8778cdb7ccfacc69fe9e1a2816af5
429 <a429@MacBook-Pro.local>
2025-09-24 09:05:10 +0300

    last recomendation

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/indi_b2b",
+  base:"/",
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['b2b.indiapp.com.ua']
+  }
 })
+
